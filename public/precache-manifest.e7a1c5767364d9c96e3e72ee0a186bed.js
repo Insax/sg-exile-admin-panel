f939aa11053cafcb0d078761ea37a72b6dec8acb
Marcus Kram <m.kram@live.at>
2021-02-21 16:28:03 +0100

    Expand Laravel Skeleton with react-create-app Skeleton

---
+++ public/precache-manifest.e7a1c5767364d9c96e3e72ee0a186bed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeab09f3536d54901f87b33618cf8955",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bc5680a6f3114674378",
+    "url": "/static/css/main.746f7a52.chunk.css"
+  },
+  {
+    "revision": "2eb5e0157f69bba5c363",
+    "url": "/static/js/2.9de80f01.chunk.js"
+  },
+  {
+    "revision": "55e028c90a240ba2cd7d832a33854dee",
+    "url": "/static/js/2.9de80f01.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9bc5680a6f3114674378",
+    "url": "/static/js/main.aa3d82a8.chunk.js"
+  },
+  {
+    "revision": "987592f7173092aa463f",
+    "url": "/static/js/runtime-main.6d94433b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
